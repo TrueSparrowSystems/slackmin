1a09f5231cdd3e2b06fe1bc8e41eeb22bac82d03
Divyajyoti Ukirde <divyajyoti.u@gmail.com>
2022-05-19 17:34:18 +0530

    fixes

--- middlewares/assignParams.js
+++ middlewares/assignParams.js
@@ -2,47 +2,16 @@ class AssignParams{
   constructor(){}
 
   /**
-   * Get request params
-   *
-   * @param {object} req
-   * @return {*}
-   */
-   _getRequestParams (req) {
-    const oThis = this;
-    // IMPORTANT NOTE: Don't assign parameters before sanitization.
-    if (req.method === 'POST') {
-      return req.body;
-    } else if (req.method === 'GET') {
-      return req.query;
-    }
-
-    return {};
-  };
-
-  /**
-   * Convert string to JSON.
+   * Assign params.
    *
    * @param {object} req
    * @param {object} res
    * @param {function} next
    */
   assignParams (req, res, next) {
-    const oThis = this;
-
-    /**
-     * IMPORTANT NOTE:
-     * Don't assign parameters before sanitization. Also override any request params, related to signatures
-     * And finally assign it to req.decodedParams
-     */
-    req.decodedParams = Object.assign(oThis._getRequestParams(req), req.decodedParams);
-
-    /**
-     * Internal decoded params are for parameters which are not passed in the request from outside.
-     * setting it to empty hash.
-     */
-    req.internalDecodedParams = {};
-
-    console.log("middleware :: assignParams :: ", req.decodedParams);
+    // IMPORTANT NOTE: Don't assign parameters before sanitization.
+    // And assign it to req.decodedParams
+    req.decodedParams = req.decodedParams || {};
 
     next();
   };
