7fd82a95cf8872cefbe4e9d635238c309bb53fb7
Kiran Kamalakar <kamalakarkiran07@gmail.com>
2024-02-14 13:41:18 +0530

    Removed logs added for debugging

--- middlewares/authentication/Signature.js
+++ middlewares/authentication/Signature.js
@@ -19,22 +19,16 @@ class ValidateSlackSignature {
   constructor(params) {
     const oThis = this;
 
-    console.log('type of params.requestBody: ------- ', typeof params.requestBody);
-
     const requestBody = params.requestBody;
     oThis.requestRawBody = params.requestRawBody;
     oThis.requestHeaders = params.requestHeaders;
 
-    console.log('requestBody.payload: ------- ', requestBody.payload);
     if (requestBody.payload) {
       const parsedPayload = JSON.parse(requestBody.payload);
-      console.log('parsedPayload.api_app_id: ------- ', parsedPayload.api_app_id);
       oThis.apiAppId = parsedPayload.api_app_id;
     } else {
       oThis.apiAppId = requestBody.api_app_id;
     }
-
-    console.log('oThis.apiAppId: ------- ', oThis.apiAppId);
   }
 
   /**
@@ -98,20 +92,13 @@ class ValidateSlackSignature {
   async _validateSignature(requestTimestamp, version, signature) {
     const oThis = this;
 
-    console.log('oThis.requestRawBody: ------- ', oThis.requestRawBody);
     const signingSecret = slackAppConstants.getSigningSecretForAppId(oThis.apiAppId);
-
-    console.log('signingSecret: ------- ', signingSecret);
 
     const signatureString = `${version}:${requestTimestamp}:${oThis.requestRawBody}`;
     const computedSignature = crypto
     .createHmac('sha256', signingSecret)
     .update(signatureString, 'utf8')
     .digest('hex');
-    
-    console.log('signatureString: ------- ', signatureString);
-    console.log('computedSignature: ------- ', computedSignature);
-    console.log('signature: ------- ', signature);
 
     if (!crypto.timingSafeEqual(Buffer.from(signature, 'utf-8'), Buffer.from(computedSignature, 'utf-8'))) {
       console.error(`Invalid signature :: ${signature}`);
